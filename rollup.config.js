fbcc34d012ee768a7685bfe31699dc4596c9a611
Roger Gee <roger.gee@tulsalibrary.org>
2021-09-02 16:10:15 -0500

    Add initial rollup build system

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+// rollup.config.js - @tccl/graph-layer-components
+
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import vue from 'rollup-plugin-vue';
+import css from 'rollup-plugin-css-only';
+
+export default {
+  plugins: [
+    nodeResolve(),
+    vue({
+      css: false
+    }),
+    css({
+      output: 'graph-layer-components.css'
+    })
+  ],
+
+  input: './src/main.js',
+  output: {
+    file: 'dist/graph-layer-components.js',
+    format: 'umd'
+  }
+};
