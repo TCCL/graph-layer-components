3a47d5a5e9547346b2048d06ce59a8799fc6bd6b
Roger Gee <roger.gee@tulsalibrary.org>
2021-10-27 17:01:50 -0500

    Update build system to produce esm bundle

--- rollup.config.js
+++ rollup.config.js
@@ -6,26 +6,33 @@ import css from "rollup-plugin-css-only";
 import svg from "rollup-plugin-svg";
 import replace from "@rollup/plugin-replace";
 
-export default {
-  plugins: [
-    nodeResolve(),
-    vue({
-      css: false
-    }),
-    css({
-      output: "graph-layer-components.css"
-    }),
-    svg(),
-    replace({
-      preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify( 'production' )
-    })
-  ],
+const plugins = [
+  nodeResolve(),
+  vue({
+    css: false
+  }),
+  css({
+    output: "graph-layer-components.css"
+  }),
+  svg(),
+  replace({
+    preventAssignment: true,
+    'process.env.NODE_ENV': JSON.stringify( 'production' )
+  })
+];
 
+export default {
+  plugins,
   input: "./src/index.js",
-  output: {
-    file: "dist/graph-layer-components.js",
-    format: "umd",
-    name: "GraphLayer"
-  }
+  output: [
+    {
+      file: "dist/graph-layer-components.js",
+      format: "umd",
+      name: "GraphLayer"
+    },
+    {
+      file: "dist/graph-layer-components.esm.js",
+      format: "es"
+    }
+  ]
 };
--- test/rollup.config.js
+++ test/rollup.config.js
@@ -6,22 +6,23 @@ import css from "rollup-plugin-css-only";
 import svg from "rollup-plugin-svg";
 import replace from "@rollup/plugin-replace";
 
-export default {
-  plugins: [
-    nodeResolve(),
-    vue({
-      css: false
-    }),
-    css({
-      output: "test.css"
-    }),
-    svg(),
-    replace({
-      preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify( 'development' )
-    })
-  ],
+const plugins = [
+  nodeResolve(),
+  vue({
+    css: false
+  }),
+  css({
+    output: "test.css"
+  }),
+  svg(),
+  replace({
+    preventAssignment: true,
+    'process.env.NODE_ENV': JSON.stringify( 'development' )
+  })
+];
 
+export default {
+  plugins,
   input: "./test/src/main.js",
   output: {
     file: "test/public/test.js",
